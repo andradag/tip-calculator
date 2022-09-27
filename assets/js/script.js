051c33c635e675b56f54c978601550082697dff5
Andrada Gacichevici <andradagacichevici@gmail.com>
2022-09-27 13:36:51 +0100

    target the HTML elements

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// target the HTML elements
+
+const formElement = document.getElementById("form");
+const formTotalInputElement = document.getElementById("total");
+const formTipInputElement = document.getElementById("tip-percentage");
+const tipSpanElement = document.getElementById("tip-amount");
+const totalSpanElement = document.getElementById("new-total");
