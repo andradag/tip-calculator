be6a99e6875e3a236b6cd849bd665f386ecb7b81
Andrada Gacichevici <andradagacichevici@gmail.com>
2022-09-27 16:05:35 +0100

    added event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,11 @@ const formTotalInputElement = document.getElementById("total");
 const formTipInputElement = document.getElementById("tip-percentage");
 const tipSpanElement = document.getElementById("tip-amount");
 const totalSpanElement = document.getElementById("new-total");
+
+const renderAmounts = (event) => {
+  event.preventDefault();
+  console.log("form submit");
+};
+
+// add event listeners
+formElement.addEventListener("submit", renderAmounts);
